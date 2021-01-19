7147f90be2d573cfd0862628f90d9fd676acc1c9
Søren Madsen <smadsen.epost@gmail.com>
2021-01-19 15:55:21 +0100

    - Prepared the script for checking length on the configuration variables in the ini-files.
    - Prepared the script for checking length on the configuration variables in the ini-files.
      Not Finished yet. Currently the functions required have been added. The checks aren't performed yet.
      - Added 3 new general settings variables.
        - These variables defines length limitations that can be configured to ensure code injection can be avoided or minimized by
          limiting the number of characters allowed in parameters to the functions.
    - No longer excluding thumbs.db from the backup archive.
    - Corrected an error that made the script exit without logical warning if the settings ini-files wasn't read and an exception occured.
      It was caused by using an undeclared variable - varBackupSettingsFileRead in the logging functions ..\logging append_to_logfile and ..\logging 
      append_newline_to_logfile.
    - Removed incorrect file reference. The function is located in the same file.
    - Added a function to calculate the string length of the parameter supplied to the function.

---
+++ Multiple_Backups.cmd
@@ -0,0 +1,54 @@
+@echo off
+REM Version and Github_upload date: 1.0 (05-12-2020)
+REM Author/Developer: Søren Madsen
+REM Github url: https://github.com/MrSmadsen/Development/tree/main/Microsoft_Batch/SimpleBackup
+REM Desciption: This is a Microsoft Batch script to automate backup and archive functionality
+REM             provided by standard archiving programs such as 7zip.
+REM             It has been developed for my personal setup and my own use case.
+REM Documentation: Checkout the file: Howto_Description.pdf
+REM Test_Disclaimer: This script has been tested on: Microsoft Windows 10 64bit home (Danish).
+REM                  Feel free to use this script/software at your own risk.
+REM File Encoding: utf-8
+
+SET "varMultipleBackups=YES"
+SET "varGeneralSettingsFile=..\Settings.ini"
+SET "varSettingsFileRead=NO"
+SET "varBackupSettingsFileRead=NO"
+
+REM  Enable this to backup the latest raspberry pi 3b+ image before the general backup.
+CALL :backupRaspberryPiImage
+REM  Most complete backup I have defined.
+CALL :fullBackup
+PAUSE
+EXIT
+
+REM Because the function :readBackupSettingsFile calls ..\fileSystem NormalizePath with a
+REM one-step navigation backtrack we have to %CD% before calling it.
+:readGeneralSettingsFile
+CALL ..\utility_functions :readBackupSettingsFile_Limits "%varGeneralSettingsFile%"
+CALL ..\utility_functions :readBackupSettingsFile "%varGeneralSettingsFile%"
+SET "varSettingsFileRead=YES"
+
+REM Set code page to unicode - Requires that the batfile is saved in unicode utf-8 format.
+chcp %varCodePage% > nul
+EXIT /B 0
+
+:backupRaspberryPiImage
+cd ".\BackupRaspberry3B+ImageLatest"
+IF "%varSettingsFileRead%"=="NO" (
+  CALL :readGeneralSettingsFile
+)
+call RaspberryBackup.cmd
+TIMEOUT /T 2
+cd ".."
+EXIT /B 0
+
+:fullBackup
+cd ".\FullBackup"
+IF "%varSettingsFileRead%"=="NO" (
+  CALL :readGeneralSettingsFile
+)
+call BackupFolders.cmd
+TIMEOUT /T 2
+cd ".."
+EXIT /B 0
---
+++ utility_functions.cmd
@@ -0,0 +1,344 @@
+@echo off
+REM Version and Github_upload date: 1.0 (05-12-2020)
+REM Author/Developer: Søren Madsen
+REM Github url: https://github.com/MrSmadsen/Development/tree/main/Microsoft_Batch/SimpleBackup
+REM Desciption: This is a Microsoft Batch script to automate backup and archive functionality
+REM             provided by standard archiving programs such as 7zip.
+REM             It has been developed for my personal setup and my own use case.
+REM Documentation: Checkout the file: Howto_Description.pdf
+REM Test_Disclaimer: This script has been tested on: Microsoft Windows 10 64bit home (Danish).
+REM                  Feel free to use this script/software at your own risk.
+REM File Encoding: utf-8
+
+REM Set code page to unicode - Requires that the batfile is saved in unicode utf-8 format.
+chcp %varCodePage% > nul
+
+REM Param_1: Function_To_Be_Called
+REM Param_2: Function_Param_1
+REM Param_3: Function_Param_2
+REM Param_4: Function_Param_3
+REM Param_5: Function_Param_4
+CALL %1 %2 %3 %4 %5
+EXIT /B 0
+
+REM Just a dummy function that does nothing.
+:do_nothing
+REM start at 1, steps by 1, until 2.
+for /l %%x IN (1,1,2) do (
+  REM Jump back to Callee.
+  EXIT /B 0
+  echo User should never see this on the console.
+)
+
+REM Alternetively
+REM IF 0==1 (
+REM   echo do everything!
+REM )
+EXIT /B 0
+
+:reset_errorlevel
+REM Echoes the windows version to device nul. This should work without error and without output hence setting errorlevel to 0.
+REM STDERR is still being sent to stderr output to know is something weird with ver happens.
+ver > nul
+EXIT /B 0
+
+REM If running as elevated admin, member of local and/or domain admin groups. Set relevant variables to NO/YES
+:is_cmd_running_with_admin_priviligies_using_whoami
+CALL :reset_errorlevel
+SET varElevatedAdminPriviligies=NO
+whoami /groups | find "S-1-16-12288" > nul 2>&1
+IF %ERRORLEVEL%==0 (
+  SET varElevatedAdminPriviligies=YES
+) ELSE IF %ERRORLEVEL%==1 (
+  SET varElevatedAdminPriviligies=NO
+) ELSE (
+  CALL :Exception_End "%varTargetLogFile%" "Unhandled exception in function :is_cmd_running_with_admin_priviligies_using_whoami - Elevations_Part" "OUTPUT_TO_STDOUT" "OUTPUT_DEBUG"
+)
+
+CALL :reset_errorlevel
+SET varUserInLocalAdministratorsGroup=NO
+whoami /groups | find "S-1-5-32-544" > nul 2>&1
+IF %ERRORLEVEL%==0 (
+  SET varUserInLocalAdministratorsGroup=YES
+) ELSE IF %ERRORLEVEL%==1 (
+  SET varUserInLocalAdministratorsGroup=NO
+) ELSE (
+  CALL :Exception_End "%varTargetLogFile%" "Unhandled exception in function :is_cmd_running_with_admin_priviligies_using_whoami - Local Admin Part" "OUTPUT_TO_STDOUT" "OUTPUT_DEBUG"
+)
+
+CALL :reset_errorlevel
+SET varUserInDomainAdministratorsGroup=NO
+whoami /groups | find "-512" > nul 2>&1
+IF %ERRORLEVEL%==0 (
+  SET varUserInDomainAdministratorsGroup=YES
+) ELSE IF %ERRORLEVEL%==1 (
+  SET varUserInDomainAdministratorsGroup=NO
+) ELSE (
+  CALL :Exception_End "%varTargetLogFile%" "Unhandled exception in function :is_cmd_running_with_admin_priviligies_using_whoami - Domain Admin Part" "OUTPUT_TO_STDOUT" "OUTPUT_DEBUG"
+)
+CALL :reset_errorlevel
+EXIT /B 0
+
+REM This function tries to setup a windows services.msc service as requested by the params:
+REM Param_1: Service name
+REM PAram_2: Service mode
+:set_windows_msc_service_start_mode
+IF [%1]==[] (
+  echo Error: No service name provided. Exitting function.
+  EXIT /B 1
+)
+  
+IF [%2]==[] (
+  echo Error: No start mode provided. Exitting function.
+  EXIT /B 1
+)
+
+echo Param1_Unmodified: %1   -   Param_1_No_Brackets: %~1
+echo Param2_Unmodified: %2   -   Param_2_No_Brackets: %~2
+
+sc config %~1 start= %~2
+EXIT /B 0
+
+REM This function tries to start a windows services.msc service as requested by the param:
+REM Param_1: Service name
+:start_windows_msc_service
+IF [%1]==[] (
+  echo Error: No service name provided. Exitting function.
+  EXIT /B 1
+)
+
+echo Param1_Unmodified: %1   -   Param_1_No_Brackets: %~1
+
+REM VisualSVN Background Job Service
+sc start %~1
+EXIT /B 0
+
+REM This function tries to stop a windows services.msc service as requested by the param:
+REM Param_1: Service name
+:stop_windows_msc_service
+IF [%1]==[] (
+  echo Error: No service name provided. Exitting function.
+  EXIT /B 1
+)
+
+echo Param1_Unmodified: %1   -   Param_1_No_Brackets: %~1
+
+REM VisualSVN Background Job Service
+sc stop %~1
+EXIT /B 0
+
+REM Param_1: Path to logFile.
+REM Param_2: Name of the command to start.
+:logTimeStampB4CommandStart
+IF [%1]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampBeforeCommandStart - No path supplied to the logfile. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%1]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampBeforeCommandStart - Empty double qoutes supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%2]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampBeforeCommandStart - Parameter 2 command/function name missing. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%2]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampBeforeCommandStart - Parameter 2 command/function name missing. Only double quotes found. Exit" "OUTPUT_TO_STDOUT" ""
+)
+
+REM ECHO param_1 %~1
+REM ECHO param_2 %~2
+
+SET varDate=%DATE:~-4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%-%TIME:~3,2%
+SET varDate=%varDate: =0%
+
+..\logging :Append_NewLine_To_LogFile "%~1" "OUTPUT_TO_STDOUT" ""
+..\logging :Append_To_LogFile "%~1" "%~2 started at: %varDate%." "OUTPUT_TO_STDOUT" ""
+EXIT /B 0
+
+REM Param_1: Path to logFile.
+REM Param_2: Name of the command to start.
+:logTimeStamp_CommandFinished
+IF [%1]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampWhenCommandIsFinished - No path supplied to the logfile. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%1]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampWhenCommandIsFinished - Empty double qoutes supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%2]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampWhenCommandIsFinished - Parameter 2 command/function name missing. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%2]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":logTimeStampWhenCommandIsFinished - Parameter 2 command/function name missing. Only double quotes found. Exit" "OUTPUT_TO_STDOUT" ""
+)
+
+SET varDate=%DATE:~-4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%-%TIME:~3,2%
+SET varDate=%varDate: =0%
+
+..\logging :Append_NewLine_To_LogFile "%~1" "OUTPUT_TO_STDOUT" ""
+..\logging :Append_To_LogFile "%~1" "%~2 finished at: %varDate%." "OUTPUT_TO_STDOUT" ""
+EXIT /B 0
+
+REM Param_1: Path to settingsfile.
+:readBackupSettingsFile_Limits
+IF NOT EXIST "%~f1" (
+  CALL :Exception_End "%varTargetLogFile%" "Settings file %~f1 does not exist. Exit." "OUTPUT_TO_STDOUT" ""
+)
+
+ECHO Retriving limit values from file: %~f1
+REM If the path argument %~1 in FOR /F is encapsulated in "" the for loop will tokenize the filename and not the file contents.
+REM Therefore the relative path is used instead of the absolute path %~f1 to avoid problems with spaces.
+FOR /f "eol=# tokens=1,2 delims==" %%i in (%~1) do (
+  IF ["%%j"]==[""] (
+    CALL :Exception_End "%varTargetLogFile%" "Empty variable found in file: %~f1. Exit." "OUTPUT_TO_STDOUT" ""
+  )
+  IF "%%i"=="varFileNameLength" (
+    SET %%i=%%j
+  )
+  IF "%%i"=="varFolderLength" (
+    SET %%i=%%j
+  )
+  IF "%%i"=="varPathLength" (
+    SET %%i=%%j
+  )
+)
+EXIT /B 0
+
+REM Param_1: Path to settingsfile.
+:readBackupSettingsFile
+IF NOT EXIST "%~f1" (
+  CALL :Exception_End "%varTargetLogFile%" "Settings file %~f1 does not exist. Exit." "OUTPUT_TO_STDOUT" ""
+)
+
+ECHO Read settings from file: %~f1
+REM If the path argument %~1 in FOR /F is encapsulated in "" the for loop will tokenize the filename and not the file contents.
+REM Therefore the relative path is used instead of the absolute path %~f1 to avoid problems with spaces.
+FOR /f "eol=# tokens=1,2 delims==" %%i in (%~1) do (
+  IF ["%%j"]==[""] (
+    CALL :Exception_End "%varTargetLogFile%" "Empty variable found in file: %~f1. Exit." "OUTPUT_TO_STDOUT" ""
+  )
+
+  IF "%%i"=="varBackupLocation" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varExistingArchivePath" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varExtractionLocation" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varSrcPathFolder01" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varSrcPathFolder02" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varDstPathFolder01" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varDstPathFolder02" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varSimpleBackupCheckoutPath" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varRepositoryLocation" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varRepositoryDumpLocation" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varSvnPath" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varSvnadminPath" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE IF "%%i"=="varArchiveProgram" (
+    CALL ..\fileSystem :NormalizeFilePath "%%j\." %%i
+  ) ELSE (
+    SET %%i=%%j
+  )
+)
+EXIT /B 0
+
+REM Inspiration from: https://ss64.com/nt/syntax-strlen.html
+REM Param_1 The string which require length calculation.
+REM Param_2 Max_Length allowed. 
+REM Param_3 Exception on error.
+REM Param_4: Verbose_Mode - "V"
+:strLength
+IF [%1]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":strLength - No value supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%1]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":strLength - Empty double qoutes supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+
+IF [%2]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":strLength - No value supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%2]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":strLength - Empty double qoutes supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+
+IF [%3]==[] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":strLength - No value supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+IF [%3]==[""] (
+  CALL :Exception_End "NO_FILE_HANDLE" ":strLength - Empty double qoutes supplied to the function. Exit" "OUTPUT_TO_STDOUT" ""
+)
+
+Setlocal EnableDelayedExpansion
+Set "s=#%~1"
+Set "len=0"
+For %%N in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+  if "!s:~%%N,1!" neq "" (
+    set /a "len+=%%N"
+    set "s=!s:~%%N!"
+  )
+)
+
+IF !len! gtr %~2 (
+  ECHO Param_1 value:      %~1
+  ECHO Calculated length:  !len!
+  ECHO Max_Length allowed: %~2
+  IF "%~3"=="YES" (	
+    ECHO :strLength - Calculated length !len! exceeds Max_Length of %~2. Exit.
+	PAUSE
+	EXIT 1
+  )
+)
+
+IF "%~4"=="V" ( ECHO Calculated length !len! exceeds Max_Length of %~2. Return to caller. )
+IF "%~4"=="v" ( ECHO Calculated length !len! exceeds Max_Length of %~2. Return to caller. )
+Setlocal DisableDelayedExpansion
+EXIT /B 0
+
+REM Exits the script and writes the error message provided.
+REM Param_1: FileHandle
+REM Param_2: Message
+REM Param_3: OUTPUT_TO_STDOUT  -  The function will also echo the message to stdout.
+REM Param_4: OUTPUT_DEBUG      - Outputs the error messages in this function.
+:Exception_End
+SET varLogToSTDOUTOK=NOT_DEFINED
+
+CALL ..\logging :Append_NewLine_To_LogFile "%~1" "%~3" "%~4"
+CALL ..\logging :Append_To_LogFile "%~1" "Exception caught." "OUTPUT_TO_STDOUT" "%~4"
+CALL ..\logging :Append_To_LogFile "%~1" "%~2" "%~3" "%~4"
+CALL ..\logging :Append_NewLine_To_LogFile "%~1" "%~3" "%~4"
+
+CALL :Cleanup "CLEANUP_DISABLED"
+CALL ..\logging :Append_To_LogFile "%~1" "Exitting." "OUTPUT_TO_STDOUT" "%~4"
+CALL ..\logging :Append_NewLine_To_LogFile "%~1" "%~3" "%~4"
+PAUSE
+EXIT
+
+REM PARAM_1 Enable the cleanup function. Values: "CLEANUP_ENABLED", "CLEANUP_DISABLED" or "CLEANUP_ASK"
+:Cleanup
+IF EXIST "%varTargetBackupfolder%" (
+  CALL ..\logging :Append_NewLine
+
+  IF "%~1"=="CLEANUP_ENABLED" (
+    rmdir /Q /S "%varTargetBackupfolder%"
+	IF %ERRORLEVEL%==0 (
+	  CALL ..\logging :Append_To_Screen "CLEANUP_ENABLED: Deleted folder %varTargetBackupfolder%" "OUTPUT_TO_STDOUT" ""
+    )
+  ) ELSE IF "%~1%"=="CLEANUP_DISABLED" (
+	  CALL ..\logging :Append_To_Screen "CLEANUP_DISABLED: No cleanup done." "OUTPUT_TO_STDOUT" ""
+  ) ELSE IF "%~1%"=="CLEANUP_ASK" (
+    CALL ..\logging :Append_To_Screen "Delete folder %varTargetBackupfolder%?" "OUTPUT_TO_STDOUT" ""
+	rmdir /S "%varTargetBackupfolder%"
+	IF %ERRORLEVEL%==0 (
+	  CALL ..\logging :Append_To_Screen "CLEANUP_ASK: Cleanup done on folder %varTargetBackupfolder%." "OUTPUT_TO_STDOUT" ""
+    )
+  ) ELSE (
+	CALL ..\logging :Append_To_Screen "CLEANUP: No cleanup done." "OUTPUT_TO_STDOUT" ""
+  )
+)
+EXIT /B 0
