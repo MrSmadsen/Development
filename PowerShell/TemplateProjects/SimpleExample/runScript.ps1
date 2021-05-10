57006fc4daef23b18cf7e3080623424f69d7501e
Søren Madsen <smadsen.epost@gmail.com>
2021-05-10 13:00:35 +0200

    - Updated runScript.ps1 for both projects.
    - Updated runScript.ps1 for both projects.

--- PowerShell/TemplateProjects/SimpleExample/runScript.ps1
+++ PowerShell/TemplateProjects/SimpleExample/runScript.ps1
@@ -1,17 +1,35 @@
-#If debugging and verbose output should be disabled: Usage: .\runScript.ps1
-#If debugging and verbose output should be enabled:  Usage:  .\runScript.ps1 -enableDebug -enableVerbose
+#------------------------------ Below here! Excample: using statement. ------------------------------
+# If debugging and verbose output should be disabled: Usage: .\runScript.ps1
+# If debugging output should be enabled:              Usage:  .\runScript.ps1 -enableDebug
+# If debugging and verbose output should be enabled:  Usage:  .\runScript.ps1 -enableDebug -enableVerbose
+
+# Using using statement and public class members. This is meant to be the scenario when using object oriented style of programming
+# and we want to avoid declaring Export-ModuleMember -Function <FunctionName> in the module.
+# It did not run correctly without extension or if pointed directly to the manifest file.
+using module .\mSimpleExample\mSimpleExample.psm1
+
 param (
     [switch]$enableDebug,
     [switch]$enableVerbose
 )
 
+$objSimpleExampleOfAClass = New-Object -TypeName SimpleExampleOfAClass -ArgumentList $enableDebug, $enableVerbose    
+# String: Double quoted strings are expandable     - Values in $-expressions inside the string is replaced by the value.
+# String: Single quoted strings are not expandable - The string is parsed "as it is" to the callee.
+$objSimpleExampleOfAClass.printStringValue('SimpleExample')
+
+#------------------------------ Below here! Function export. ------------------------------
 #Importing module:
 # For running when working:
 
 # Starting the script using the manifest file is still abit buggy, but it does run if your execution policy is set up to allow scripts.
 # Import-Module -Name .\mSimpleExample\mSimpleExample.psd1
 
-Import-Module -Name .\mSimpleExample\mSimpleExample.psm1
+# TEST:
+# Should load manifest file if it is present and has the correct name (same as sub-folder and module itself)
+#Import-Module .\mSimpleExample\mSimpleExample
+#Import-Module -Name .\mSimpleExample\mSimpleExample
+#Import-Module -Name .\mSimpleExample\mSimpleExample.psm1
 
 # For debugging: -Force will reimport the module without having to restart the powershell session.  
   # Import-Module -Name .\mSimpleExample\mSimpleExample.psm1 -verbose -Force
@@ -21,7 +39,7 @@ Import-Module -Name .\mSimpleExample\mSimpleExample.psm1
   #Import-Module -Name .\mSimpleExample\mSimpleExample.psd1 -verbose -Force
   #Get-Module .\mSimpleExample\mSimpleExample.psd1 -ListAvailable
     
-userModule_SimpleExample_main $enableDebug $enableVerbose
+# userModule_SimpleExample_main $enableDebug $enableVerbose
 
 # Remove-Module -Name .\mSimpleExample\mSimpleExample.psm1
 # Remove-Module -Name .\mSimpleExample\mSimpleExample.psd1
--- PowerShell/TemplateProjects/SimpleExampleWithLogFunction/runScript.ps1
+++ PowerShell/TemplateProjects/SimpleExampleWithLogFunction/runScript.ps1
@@ -1,17 +1,31 @@
-#If debugging and verbose output should be disabled: Usage: .\runScript.ps1
-#If debugging and verbose output should be enabled:  Usage:  .\runScript.ps1 -enableDebug -enableVerbose
+#------------------------------ Below here! Excample: using statement. ------------------------------
+# If debugging and verbose output should be disabled: Usage: .\runScript.ps1
+# If debugging output should be enabled:              Usage:  .\runScript.ps1 -enableDebug
+# If debugging and verbose output should be enabled:  Usage:  .\runScript.ps1 -enableDebug -enableVerbose
+
+# Using using statement and public class members. This is meant to be the scenario when using object oriented style of programming
+# and we want to avoid declaring Export-ModuleMember -Function <FunctionName> in the module.
+# It did not run correctly without extension or if pointed directly to the manifest file.
+using module .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psm1
+
 param (
     [switch]$enableDebug,
     [switch]$enableVerbose
 )
 
+$objSimpleExampleWithLogFunctionClass = New-Object -TypeName SimpleExampleWithLogFunctionClass -ArgumentList $enableDebug, $enableVerbose    
+# String: Double quoted strings are expandable     - Values in $-expressions inside the string is replaced by the value.
+# String: Single quoted strings are not expandable - The string is parsed "as it is" to the callee.
+$objSimpleExampleWithLogFunctionClass.printStringValue('SimpleExampleWithLogFunction')
+
+#------------------------------ Below here! Function export. ------------------------------
 #Importing module:
 # For running when working:
 
 # Starting the script using the manifest file is still abit buggy, but it does run if your execution policy is set up to allow scripts.
 # Import-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psd1
 
-Import-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psm1
+#Import-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psm1
 
 # For debugging: -Force will reimport the module without having to restart the powershell session.
   # Import-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psm1 -verbose -Force
@@ -21,7 +35,7 @@ Import-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunctio
   #Import-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psd1 -verbose -Force
   #Get-Module .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psd1 -ListAvailable
     
-userModule_SimpleExampleWithLogFunction_main $enableDebug $enableVerbose
+#userModule_SimpleExampleWithLogFunction_main $enableDebug $enableVerbose
 
 # Remove-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psm1
 # Remove-Module -Name .\mSimpleExampleWithLogFunction\mSimpleExampleWithLogFunction.psd1
